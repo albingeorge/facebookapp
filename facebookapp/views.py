99a19394a969accf73ec182502fd177aa7da6116
stRIker <albinin0002@gmail.com>
2013-10-01 11:01:04 +0530

    Facebook get from graph

--- facebookapp/views.py
+++ facebookapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.template import Template, Context
 from facebookapp.forms import FacebookForm
 import requests
+import re
 
 def home(request):
 	d = {'title': 'FacebookApp :: Input Facebook Url'}
@@ -16,6 +17,11 @@ def home(request):
 def getCompany(request):
 	d = {'title': 'FacebookApp :: Company Details'}
 	url = request.GET.get('company', '')
-	companyName = 
+	m = re.match(r".*facebook.com/(?P<company>\w+)/*.*", url)
+	try:
+		companyName = m.group(1)
+	except(AttributeError):
+		companyName = 'Invalid URL'
 	company = requests.get("https://graph.facebook.com/" + companyName)
+	d['text'] = company.text
 	return render(request, 'companyDetails.html', d)
