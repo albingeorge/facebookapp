17eeccbf8c2b0775afdc9140a33022fdb601d2a0
striker@accent <albinin0002@gmail.com>
2013-10-01 15:10:52 +0530

    Added facebook retrieval functionalities

--- facebookapp/views.py
+++ facebookapp/views.py
@@ -4,24 +4,35 @@ from django.template import Template, Context
 from facebookapp.forms import FacebookForm
 import requests
 import re
+import json
 
 def home(request):
 	d = {'title': 'FacebookApp :: Input Facebook Url'}
 	form = FacebookForm(request.POST or None)
 	if form.is_valid():
 		data = form.cleaned_data
-		return HttpResponseRedirect('/companyDetails/?company=' + data['url'])
+		m = re.match(r".*facebook.com/(?P<company>\w+)/*.*", data['url'])
+		try:
+			companyName = m.group(1)
+		except(AttributeError):
+			companyName = 'Invalid URL'
+		return HttpResponseRedirect('/companyDetails/?company=' + companyName)
 	d['form'] = form
 	return render(request, 'home.html', d)
 
 def getCompany(request):
 	d = {'title': 'FacebookApp :: Company Details'}
-	url = request.GET.get('company', '')
-	m = re.match(r".*facebook.com/(?P<company>\w+)/*.*", url)
-	try:
-		companyName = m.group(1)
-	except(AttributeError):
-		companyName = 'Invalid URL'
-	company = requests.get("https://graph.facebook.com/" + companyName)
-	d['text'] = company.text
+	companyName = request.GET.get('company', '')
+	if companyName == 'Invalid URL':
+		company = companyName
+	else:
+		d['companyName'] = companyName
+		try:
+			company = requests.get("https://graph.facebook.com/" + companyName)
+			company = json.loads(company.text)
+			name = company['name']
+		except:
+			company = 'Something went wrong'
+
+	d['text'] = name
 	return render(request, 'companyDetails.html', d)
